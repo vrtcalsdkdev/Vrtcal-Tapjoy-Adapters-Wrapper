cc88b90bfef3ad2f775e4fafbfdf64f7d00c7ace
Scott McCoy <scotthmccoy@gmail.com>
2023-10-11 12:29:51 -0700

    Initial Commit

---
+++ Vrtcal-Tapjoy-Adapters-Wrapper.podspec
@@ -0,0 +1,22 @@
+Pod::Spec.new do |s|
+    s.name         = "Vrtcal-Tapjoy-Adapters-Wrapper"
+    s.version      = "1.0.0"
+    s.summary      = "Wrapper for Vrtcal-AppLovin-Adapters"
+    s.homepage     = "http://vrtcal.com"
+    s.license = { :type => 'Copyright', :text => <<-LICENSE
+                   Copyright 2020 Vrtcal Markets, Inc.
+                  LICENSE
+                }
+    s.author       = { "Scott McCoy" => "scott.mccoy@vrtcal.com" }
+    
+    s.source       = { :git => "https://github.com/vrtcalsdkdev/Vrtcal-Tapjoy-Adapters-Wrapper.git", :tag => "#{s.version}" }
+    s.source_files = "*.swift"
+
+    s.platform = :ios
+    s.ios.deployment_target  = '11.0'
+
+    s.dependency 'Vrtcal-Adapters-Wrapper-Parent'
+    s.dependency 'Vrtcal-Tapjoy-Adapters'
+
+    s.static_framework = true
+end
---
+++ VrtcalTapjoyAdaptersWrapper.swift
@@ -0,0 +1,139 @@
+import Tapjoy
+import Vrtcal_Adapters_Wrapper_Parent
+
+// Must be NSObject for TJPlacementDelegate
+class VrtcalTapjoyAdaptersWrapper: NSObject, AdapterWrapperProtocol {
+    
+    var appLogger: Logger
+    var sdkEventsLogger: Logger
+    var delegate: AdapterWrapperDelegate
+    var tjPlacement: TJPlacement?
+    
+    required init(
+        appLogger: Logger,
+        sdkEventsLogger: Logger,
+        delegate: AdapterWrapperDelegate
+    ) {
+        self.appLogger = appLogger
+        self.sdkEventsLogger = sdkEventsLogger
+        self.delegate = delegate
+    }
+    
+    func initializeSdk() {
+        appLogger.log()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(tjcConnectSuccess(notif:)),
+            name: NSNotification.Name(rawValue: TJC_CONNECT_SUCCESS), 
+            object: nil
+        )
+            
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(tjcConnectFail(notif:)),
+            name: NSNotification.Name(rawValue: TJC_CONNECT_FAILED),
+            object: nil
+        )
+        
+        Tapjoy.setDebugEnabled(true)
+        Tapjoy.connect("mbAVpNXZSzC0d9I0lTHXawEBBFpgzq63v8WLQZDHxEUgwXPlk5bvyAXjK4Sl")
+    }
+    
+    func handle(vrtcalAsSecondaryConfig: VrtcalAsSecondaryConfig) {
+        
+        switch vrtcalAsSecondaryConfig.placementType {
+                
+            case .banner:
+                appLogger.log("Tapjoy does not support banners")
+
+            case .interstitial:
+                appLogger.log("Tapjoy Interstitial")
+                
+                guard let tjPlacement = TJPlacement(
+                    name: vrtcalAsSecondaryConfig.adUnitId,
+                    delegate: self
+                ) else {
+                    sdkEventsLogger.log("No placement")
+                    return
+                }
+                tjPlacement.videoDelegate = self
+                tjPlacement.requestContent()
+                self.tjPlacement = tjPlacement
+                
+            case .rewardedVideo:
+                fatalError("rewardedVideo not supported for TapJoy")
+                
+            case .showDebugView:
+                appLogger.log("TapJoy doesn't have a debug view")
+        }
+    }
+    
+    func showInterstitial() -> Bool {
+        if let tjPlacement {
+            tjPlacement.showContent(with: delegate.viewController)
+            return true
+        }
+        
+        return false
+    }
+}
+
+extension VrtcalTapjoyAdaptersWrapper {
+    @objc func tjcConnectSuccess(notif: NSNotification) {
+        appLogger.log()
+    }
+
+    @objc func tjcConnectFail(notif: NSNotification) {
+        appLogger.log()
+    }
+}
+
+
+extension VrtcalTapjoyAdaptersWrapper: TJPlacementDelegate {
+    func requestDidSucceed(_ placement: TJPlacement) {
+        sdkEventsLogger.log("Tapjoy requestDidSucceed")
+    }
+    
+    func requestDidFail(_ placement: TJPlacement, error: Error?) {
+        sdkEventsLogger.log("Tapjoy requestDidFail: \(String(describing: error))")
+    }
+    
+    func contentIsReady(_ placement: TJPlacement) {
+        sdkEventsLogger.log("Tapjoy contentIsReady")
+    }
+
+    func contentDidAppear(_ placement: TJPlacement) {
+        sdkEventsLogger.log("Tapjoy contentDidAppear")
+    }
+    
+    func contentDidDisappear(_ placement: TJPlacement) {
+        sdkEventsLogger.log("Tapjoy contentDidDisappear")
+    }
+
+    func didClick(_ placement: TJPlacement) {
+        sdkEventsLogger.log("Tapjoy didClick")
+    }
+    
+    func placement(_ placement: TJPlacement, didRequestPurchase request: TJActionRequest?, productId: String?) {
+        sdkEventsLogger.log("Tapjoy placement didRequestPurchase")
+    }
+
+    func placement(_ placement: TJPlacement, didRequestReward request: TJActionRequest?, itemId: String?, quantity: Int32) {
+        sdkEventsLogger.log("Tapjoy placement didRequestReward")
+    }
+}
+
+
+extension VrtcalTapjoyAdaptersWrapper: TJPlacementVideoDelegate {
+    func videoDidStart(_ placement: TJPlacement) {
+        appLogger.log()
+    }
+    
+    func videoDidComplete(_ placement: TJPlacement) {
+        appLogger.log()
+    }
+    
+    func videoDidFail(_ placement: TJPlacement, error errorMsg: String?) {
+        appLogger.log("error: \(String(describing: errorMsg))")
+    }
+}
